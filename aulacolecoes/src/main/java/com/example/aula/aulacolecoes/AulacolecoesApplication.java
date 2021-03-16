610c3c76526f24bbecab94dadeb0e6bc654b9927
LuziaAm <luzia.castro29@gmail.com>
2021-03-16 01:01:46 -0300

    Aula List

---
+++ aulacolecoes/src/main/java/com/example/aula/aulacolecoes/AulacolecoesApplication.java
@@ -0,0 +1,13 @@
+package com.example.aula.aulacolecoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulacolecoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulacolecoesApplication.class, args);
+	}
+
+}
---
+++ aulacolecoes/src/test/java/com/example/aula/aulacolecoes/AulacolecoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula.aulacolecoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulacolecoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

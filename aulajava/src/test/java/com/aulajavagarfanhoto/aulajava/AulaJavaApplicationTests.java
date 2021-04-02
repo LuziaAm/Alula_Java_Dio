8cb6ad62dd2d1c0acbeecff41c8ba82b39a87ce2
Luzia de Castro Amorim <luzia.castro29@gmail.com>
2021-04-01 21:26:59 -0300

    Add files via upload
    Aula Java Dio

---
+++ aulajava/src/test/java/com/aulajavagarfanhoto/aulajava/AulaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulajavagarfanhoto.aulajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
